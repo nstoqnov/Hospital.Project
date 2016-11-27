e30bb6326b604f4cb37219331561d2b0ad7bddce
Nikolai Stoqnov <stoqnovn@yahoo.com>
2016-11-27 21:16:55 +0200

    pom
    pom.xml configuration

---
+++ src/main/java/hospitalweb/SpringWebApp.java
@@ -0,0 +1,11 @@
+package hospitalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebApp.class, args);
+    }
+}
